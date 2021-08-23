e30385343edee5ea38f8ad4dc8176db16dac8dd3
luizsilva <luizfsilva.091595@gmail.com>
2021-08-23 10:22:02 -0400

    added a create and delete endpoint

--- routes/index.js
+++ routes/index.js
@@ -16,9 +16,48 @@ router.post('/workouts', (req, res) => {
 
 router.patch('/workouts/:level/:exerciseName', (req, res) => {
   const { level, exerciseName } = req.params
+  const { completed } = req.body
   Workout.findOneAndUpdate({ level: level, "exercise.name": exerciseName }, 
-    { $set: { "exercise.$.completed": true } })
+    { $set: { "exercise.$.completed": completed } })
     .then((docRef) => res.send(docRef))
+})
+
+router.post('/workouts/create', (req, res) => {
+  const { workout, level } = req.body
+  console.log('This is the body from the POST request', req.body)
+
+  Workout.findOne({ level })
+  .then(foundWorkout => {
+    foundWorkout.exercise.find((exerc) => {
+      if(exerc.name === workout.name) {
+        res.status(400).send({message: `Workout with name ${workout.name} already exists`})
+        return true
+      } else {
+        console.log('This is the workout found by the level in the DB', foundWorkout)
+        foundWorkout.exercise.push(workout)
+        foundWorkout.save()
+        res.status(200).send({message: `Successfully created a new exercise for level ${level}`, workout })
+      }
+    })
+  })
+})
+
+router.post('workouts/delete', (req, res) => {
+  const { workoutName, level } = req.body
+  console.log('This is the body from the POST request', req.body)
+
+  Workout.findOne({ level })
+  .then(foundWorkout => {
+    foundWorkout.exercise.find((exerc, index) => {
+      if(exerc.name === workoutName) {
+        foundWorkout.splice(index, 1)
+        res.status(200).send({message: `Successfully deleted ${workoutName} from ${level}`})
+      } else {
+        res.status(400).send({message: `Workout with name ${workout.name} doesnt exists`})
+        return true
+      }
+    })
+  })
 })
 
 module.exports = router
